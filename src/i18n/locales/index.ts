d5678da90f55d8027fdba478341da1945b9c472a
Yousef Elsayed <yousef4511@outlook.com>
2025-01-07 03:30:54 +0200

    feat: add AR i18n locale

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,8 +1,10 @@
 import type { I18nStrings } from "../types";
+import { ARLocale } from "./ar";
 import englishLocale from "./en";
 
 export const localeToTranslations: Record<string, I18nStrings> = {
   en: englishLocale,
+  ar: ARLocale,
 };
 
 export type supportedLocales = keyof typeof localeToTranslations;
