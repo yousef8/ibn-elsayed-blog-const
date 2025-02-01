f817bb25234600f0f948ededacababac15d18b11
Yousef Elsayed <yousef4511@outlook.com>
2025-02-01 13:52:10 +0200

    fix: use localized site title in RSS

--- src/pages/ar/rss.xml.ts
+++ src/pages/ar/rss.xml.ts
@@ -3,12 +3,15 @@ import { getCollection } from "astro:content";
 import getSortedPosts from "@utils/getSortedPosts";
 import { SITE } from "@config";
 import getRelativePath from "@utils/getRelativePath";
+import { translateFor } from "@i18n/utils";
+
+const t = translateFor("ar");
 
 export async function GET() {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
   return rss({
-    title: SITE.title,
+    title: t("site.title"),
     description: SITE.desc,
     site: SITE.website,
     items: sortedPosts.map(({ data, slug }) => ({
